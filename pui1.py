d6de48a020e3e452586958b5645a974b2b40caa5
Dalibor Fonović <dalibor.fonovic@unipu.hr>
2021-01-01 09:39:32 +0100

    Initial commit

---
+++ pui1.py
@@ -0,0 +1,8 @@
+a = eval(input('unesi broj a:'))
+if a < 0:
+	print("Broj je negativan")
+	print("Apsolutna vrijednost: ", -a)
+else:
+	print("Broj je pozitivan")
+	print("Apsolutna vrijednost je: ", a)
+print("Kraj")
