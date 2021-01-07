81033f4e8fd2abef700caa3f04320c6af68dd0e4
Dalibor Fonović <dalibor.fonovic@unipu.hr>
2021-01-07 08:37:47 +0100

    izbrisan dio na početku

--- pui1.py
+++ pui1.py
@@ -1,11 +1,3 @@
-a = eval(input('unesi broj a:'))
-if a < 0:
-	print("Broj je negativan")
-	print("Apsolutna vrijednost: ", -a)
-else:
-	print("Broj je pozitivan")
-	print("Apsolutna vrijednost je: ", a)
-print("Kraj")
 '''
 #primjer zamjene dvije verijable
 x = int(input('unesite prvi broj:'))
